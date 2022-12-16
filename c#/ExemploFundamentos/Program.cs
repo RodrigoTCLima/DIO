9cf28bbfe5e6a48ed6ce6d53818d1eddf279dd3c
rodrigo <rodrigotiagocl@gmail.com>
2022-12-16 00:43:18 -0300

    adicionando comentário sobre convenções

--- c#/ExemploFundamentos/Program.cs
+++ c#/ExemploFundamentos/Program.cs
@@ -1,2 +1,10 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using ExemploFundamentos.Models;
+
+//Nome da Classe, Atributos e Métodos começa com Maiuscula, variavel com minuscula
+Pessoa pessoa1 = new Pessoa();
+
+pessoa1.Nome = "Buta";
+pessoa1.Idade = 26;
+pessoa1.Apresentar();
+
+Pessoa pessoaFisicaRepresentacao = new Pessoa();
