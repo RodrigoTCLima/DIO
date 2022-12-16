9f630603e50e113237e5341afacea6945727d6ff
rodrigo <rodrigotiagocl@gmail.com>
2022-12-16 01:13:20 -0300

    Estudo sobre variáveis básicas

--- c#/ExemploFundamentos/Program.cs
+++ c#/ExemploFundamentos/Program.cs
@@ -1,10 +1,33 @@
 ﻿using ExemploFundamentos.Models;
-
 //Nome da Classe, Atributos e Métodos começa com Maiuscula, variavel com minuscula
-Pessoa pessoa1 = new Pessoa();
 
-pessoa1.Nome = "Buta";
-pessoa1.Idade = 26;
-pessoa1.Apresentar();
+string apresentacao = "Olá, seja bem vindo";
 
-Pessoa pessoaFisicaRepresentacao = new Pessoa();
+int quantidade = 1;
+Console.WriteLine("Valor da variável quantidade: " + quantidade);
+
+quantidade = 10;
+Console.WriteLine("Valor da variável quantidade: " + quantidade);
+
+
+
+double altura = 1.80;
+
+decimal preco = 1.80M;
+
+bool condicao = true;
+
+Console.WriteLine(apresentacao);
+Console.WriteLine("Valor da variável quantidade: " + quantidade);
+Console.WriteLine("Valor da variável altura: " + altura.ToString("0.00"));
+Console.WriteLine("Valor da variável preco: " + preco);
+Console.WriteLine("Valor da variável condicao: " + condicao);
+
+
+// Pessoa pessoa1 = new Pessoa();
+
+// pessoa1.Nome = "Buta";
+// pessoa1.Idade = 26;
+// pessoa1.Apresentar();
+
+// Pessoa pessoaFisicaRepresentacao = new Pessoa();
