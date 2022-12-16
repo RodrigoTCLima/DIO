6b53b233cad83b12b5a5de3c5124e816bad7c1d7
rodrigo <rodrigotiagocl@gmail.com>
2022-12-16 01:17:12 -0300

    Conhecendo o tipo DateTime

--- c#/ExemploFundamentos/Program.cs
+++ c#/ExemploFundamentos/Program.cs
@@ -1,27 +1,31 @@
 ﻿using ExemploFundamentos.Models;
 //Nome da Classe, Atributos e Métodos começa com Maiuscula, variavel com minuscula
 
-string apresentacao = "Olá, seja bem vindo";
-
-int quantidade = 1;
-Console.WriteLine("Valor da variável quantidade: " + quantidade);
-
-quantidade = 10;
-Console.WriteLine("Valor da variável quantidade: " + quantidade);
+DateTime dataAtual = DateTime.Now.AddDays(5);
+Console.WriteLine(dataAtual.ToString("dd/MM/yyyy HH:mm"));
 
 
 
-double altura = 1.80;
+// string apresentacao = "Olá, seja bem vindo";
 
-decimal preco = 1.80M;
+// int quantidade = 1;
+// Console.WriteLine("Valor da variável quantidade: " + quantidade);
 
-bool condicao = true;
+// quantidade = 10;
+// Console.WriteLine("Valor da variável quantidade: " + quantidade);
 
-Console.WriteLine(apresentacao);
-Console.WriteLine("Valor da variável quantidade: " + quantidade);
-Console.WriteLine("Valor da variável altura: " + altura.ToString("0.00"));
-Console.WriteLine("Valor da variável preco: " + preco);
-Console.WriteLine("Valor da variável condicao: " + condicao);
+
+// double altura = 1.80;
+
+// decimal preco = 1.80M;
+
+// bool condicao = true;
+
+// Console.WriteLine(apresentacao);
+// Console.WriteLine("Valor da variável quantidade: " + quantidade);
+// Console.WriteLine("Valor da variável altura: " + altura.ToString("0.00"));
+// Console.WriteLine("Valor da variável preco: " + preco);
+// Console.WriteLine("Valor da variável condicao: " + condicao);
 
 
 // Pessoa pessoa1 = new Pessoa();
