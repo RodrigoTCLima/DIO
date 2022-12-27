b32f2e44771538b5ce83d0fe4037e1a736b23f79
rodrigo <rodrigotiagocl@gmail.com>
2022-12-26 22:38:20 -0300

    incremento e decremento

--- c#/ExemploFundamentos/Program.cs
+++ c#/ExemploFundamentos/Program.cs
@@ -1,16 +1,32 @@
 ﻿using ExemploFundamentos.Models;
 //Nome da Classe, Atributos e Métodos começa com Maiuscula, variavel com minuscula
 
-Calculadora calc = new Calculadora();
+int numeroIncremento = 10;
+
+Console.WriteLine(numeroIncremento);
+
+Console.WriteLine("Incrementando o 10");
+//numero = numero + 1;
+numeroIncremento++;
+
+int numeroDecremento = 20;
+Console.WriteLine("Decrementando o 20");
+//numero = numero + 1;
+numeroDecremento--;
+
+Console.WriteLine(numeroDecremento);
+
+
+//Calculadora calc = new Calculadora();
 
 // calc.Somar(10, 30);
 // calc.Subtrair(10, 50);
 // calc.Multiplicar(15, 45);
 // calc.Dividir(2, 2);
 // calc.Potencia(3, 3);
-calc.Seno(30);
-calc.Cosseno(30);
-calc.Tangente(30);
+// calc.Seno(30);
+// calc.Cosseno(30);
+// calc.Tangente(30);
 
 // bool choveu = false;
 // bool estaTarde = false;
