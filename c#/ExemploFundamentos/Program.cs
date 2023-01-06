8734a837ed60901c255bbeb147474af90b30c5f5
rodrigo <rodrigotiagocl@gmail.com>
2023-01-06 20:17:25 -0300

    estudo sobre estruturas de repetição

--- c#/ExemploFundamentos/Program.cs
+++ c#/ExemploFundamentos/Program.cs
@@ -1,8 +1,50 @@
 ﻿using ExemploFundamentos.Models;
 //Nome da Classe, Atributos e Métodos começa com Maiuscula, variavel com minuscula
 
+int soma = 0, numero = 0;
 
-Calculadora calc = new Calculadora();
+
+do
+{
+    Console.WriteLine("Digite um número (0 para parar)");
+    numero = Convert.ToInt32(Console.ReadLine());
+
+    soma += numero;
+
+} while(numero != 0);
+
+Console.WriteLine($"Total da soma dos números digitados é: {soma}");
+
+
+
+
+
+
+
+
+
+
+
+// int numero = 5;
+// int contador = 1;
+// 
+// while (contador <= 10){
+    // Console.WriteLine($"{contador}º Execução: {numero} x {contador} = {numero * contador}");
+    // contador++;
+// 
+    // if(contador == 6)
+    // {
+    // break;
+    // }
+// }
+
+// int numero = 5;
+// 
+// for (int contador = 0; contador <= 10; contador++)
+// {
+    // Console.WriteLine($"{numero} x {contador} = {numero * contador}");
+// }
+// Calculadora calc = new Calculadora();
 
 // calc.Somar(10, 30);
 // calc.Subtrair(10, 50);
@@ -12,7 +54,7 @@ Calculadora calc = new Calculadora();
 // calc.Seno(30);
 // calc.Cosseno(30);
 // calc.Tangente(30);
-calc.RaizQuadrada(9);
+// calc.RaizQuadrada(9);
 
 //int numeroIncremento = 10;
 
