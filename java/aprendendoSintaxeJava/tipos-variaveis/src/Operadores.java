b02623eeca520b622700c3c141b4063513831257
Rodrigo Tiago <rodrigotiagocl@gmail.com>
2023-03-14 21:16:46 -0300

    Adicionando estudo sobre operadores

---
+++ java/aprendendoSintaxeJava/tipos-variaveis/src/Operadores.java
@@ -0,0 +1,75 @@
+public class Operadores {
+    public static void main(String[] args) {
+        
+        /*         boolean condicao1 = true;
+        boolean condicao2 = false;
+
+        if(condicao1 && (7>4)) System.out.println("verdade");
+        if(condicao1 && condicao2){
+            System.out.println("verdadeiras");
+
+        }
+        if(condicao1 || condicao2){
+            System.out.println("Pelo menos uma é verdadeira");
+        }
+        System.out.println("Fim"); */
+
+        /*         String nome1 = "Rodrigo";
+        String nome2 = new String("Rodrigo");
+
+        System.out.println(nome1.equals(nome2));
+
+        int numero1 = 1;
+        int numero2 = 2;
+
+        boolean simNao = numero1 == numero2;
+
+        System.out.println("numero1 é igual o numero2 ? " +simNao);
+
+        simNao = numero1 != numero2;
+
+        System.out.println("numero1 é diferente o numero2 ? " + simNao);
+
+        simNao = numero1 > numero2;
+
+        System.out.println("numero1 é maior que o numero2 ?" +simNao);
+ */
+
+        /*         int a, b;
+
+        a = 6;
+        b = 6;
+
+        String resultado = a==b ? "verdadeiro" :  "false";
+
+        System.out.println(resultado); */
+
+        //boolean variavel = true;
+
+        //System.out.println(!variavel);
+
+        //variavel = !variavel;
+
+        //System.out.println(variavel);
+
+        //        int numero = 5;
+
+//        numero = numero +2;
+
+        //System.out.println(numero);
+        
+        //numero++;
+        
+        //System.out.println(numero);
+
+        //int numero = 5;
+        //System.out.println(numero);
+        //numero = -numero;
+        //System.out.println(numero);
+        //numero = -numero;
+        //System.out.println(numero);
+
+        //String nomeCompleto = "LINGUAGEM" + "JAVA";
+        //System.out.println(nomeCompleto);
+    }
+}
